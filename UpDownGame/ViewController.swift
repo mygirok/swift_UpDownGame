98d6fad26b83ddd80d93c9f38e8369b0f2a3967e
mygirok <forgirok@gmail.com>
2021-12-01 20:23:50 +0900

    UpDownGame start

---
+++ UpDownGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by forworkyong on 2021/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
