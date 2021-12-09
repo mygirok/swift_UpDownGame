9afa18949a8e75bed3d0afccb92868ad70972ad8
mygirok <forgirok@gmail.com>
2021-12-09 21:22:46 +0900

    Change value to another type, Showing Alerts

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -26,10 +26,48 @@ class ViewController: UIViewController {
     
     @IBAction func sliderVallueChanged(_ sender: UISlider) {
         print(sender.value)
+        let intergerValue: Int = Int(sender.value)
+        sliderValueLabel.text = String(intergerValue)
+    }
+    
+    func showAlert(message: String) {
+        
+        let alert = UIAlertController(title: nil,
+                                      message: message,
+                                      preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK",
+                                     style: .default) { action in
+                                        self.reset()
+        }
+        
+        alert.addAction(okAction)
+        present(alert,
+                animated: true,
+                completion: nil)
     }
     
     @IBAction func touchUpHitButton(_ sender: UIButton) {
         print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount = tryCount + 1
+        tryCountLabel.text = "\(tryCount) / 5"
+        
+        if randomValue == hitValue {
+            showAlert(message: "YOU HIT!!")
+            reset()
+        } else if tryCount >= 5 {
+            showAlert(message: "You lose...")
+            reset()
+        } else if randomValue > hitValue {
+            slider.minimumValue = Float(hitValue)
+            minimumValueLabel.text = String(hitValue)
+        } else {
+            slider.maximumValue = Float(hitValue)
+            maximumValueLabel.text = String(hitValue)
+        }
     }
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
