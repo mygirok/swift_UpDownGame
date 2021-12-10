1fe6e4f264d0012f4bde2183035d42e9bee7b70f
mygirok <forgirok@gmail.com>
2021-12-10 22:21:38 +0900

    Storyboard Seque, Close button

---
+++ UpDownGame/CreditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CreditViewController.swift
+//  CreditViewController
+//
+//  Created by forworkyong on 2021/12/10.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+    
+    @IBAction func touchUoDismissButton(_ sender: UIButton) {
+        dismiss(animated: true,
+                completion: nil)
+    }
+}
